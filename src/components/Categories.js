c25b4d1927472f989b0c2e2bfb60192054eb521b
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-08 21:24:25 +0100

    Add functionality to  display status message

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatusAction } from '../redux/categories/categories';
+
 function Categories() {
+  const dispatch = useDispatch();
+  const statusMsg = useSelector((state) => state.categories);
+
+  const statusCheck = () => {
+    dispatch(checkStatusAction());
+  };
+
   return (
     <div className="categories">
-      <button type="button">CHECK STATUS</button>
+      <button type="button" onClick={() => statusCheck()}>CHECK STATUS</button>
+      <span className="checkMsg">{statusMsg}</span>
     </div>
   );
 }
