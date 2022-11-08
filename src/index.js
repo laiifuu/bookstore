daeee58fc375cf58c0c0d3e59ad5a6e736776431
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-08 10:52:35 +0100

    Wrap the app component with a Provider component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/configureStore";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
