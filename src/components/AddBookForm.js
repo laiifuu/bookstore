abe0578e180e7039322f2355b7ab40f2db8b22eb
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-08 21:21:39 +0100

    Add functionality to add a new book

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,14 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
+import { addBookAction } from '../redux/books/books';
 
-const AddBookForm = () => (
-  <section className="add-book-form">
-    <h2>ADD NEW BOOK</h2>
-    <form className="books-list">
-      <input type="text" placeholder="Book title" />
+const AddBookForm = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
-      <button type="button">ADD BOOK</button>
-    </form>
-  </section>
-);
+  const returnState = (e) => {
+    e.preventDefault();
+    const authorInput = e.target.querySelector('.authorInput');
+    const titleInput = e.target.querySelector('.titleInput');
+    const obj = {
+      id: books.length,
+      author: authorInput.value,
+      title: titleInput.value,
+    };
+    dispatch(addBookAction(obj));
+  };
+
+  return (
+    <section className="add-book-form">
+      <h2>ADD NEW BOOK</h2>
+      <form className="books-list" onSubmit={(e) => returnState(e)}>
+        <input className="titleInput" type="text" placeholder="Book title" required />
+        <input className="authorInput" type="text" placeholder="Book author" required />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
 
 export default AddBookForm;
