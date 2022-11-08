ffa92313755e55c8313cab7a8846ad1de78524a8
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-08 22:49:14 +0100

    Use local state to get the values of the inputs

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,29 +1,51 @@
 import { useDispatch, useSelector } from 'react-redux';
-import React from 'react';
+import React, { useState } from 'react';
 import { addBookAction } from '../redux/books/books';
 
 const AddBookForm = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
   const returnState = (e) => {
     e.preventDefault();
-    const authorInput = e.target.querySelector('.authorInput');
-    const titleInput = e.target.querySelector('.titleInput');
     const obj = {
       id: books.length,
-      author: authorInput.value,
-      title: titleInput.value,
+      author,
+      title,
     };
     dispatch(addBookAction(obj));
+    e.target.reset();
   };
 
   return (
     <section className="add-book-form">
+      12,n
       <h2>ADD NEW BOOK</h2>
       <form className="books-list" onSubmit={(e) => returnState(e)}>
-        <input className="titleInput" type="text" placeholder="Book title" required />
-        <input className="authorInput" type="text" placeholder="Book author" required />
+        <input
+          className="titleInput"
+          type="text"
+          placeholder="Book title"
+          onChange={(e) => handleTitleChange(e)}
+          required
+        />
+        <input
+          className="authorInput"
+          type="text"
+          placeholder="Book author"
+          onChange={(e) => handleAuthorChange(e)}
+          required
+        />
         <button type="submit">ADD BOOK</button>
       </form>
     </section>
