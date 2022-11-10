34d7a484dc6b09a71d4d5cf0fea9a2b60ff843b1
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-10 12:15:08 +0100

    Dispatch fetchBooks function

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import AddBookForm from './AddBookForm';
 import BooksList from './BooksList';
+import { fetchBooks } from '../redux/books/books';
 
 const BooksPage = () => {
+  const dispatch = useDispatch();
+
   const list = useSelector((state) => state.books);
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   return (
     <>
       <BooksList booksList={list} />
